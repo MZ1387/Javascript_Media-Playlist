2a9920cf5284c7d25769e3bac99bbb5fa605ef53
Mario Zamora <maza1387@gmail.com>
2017-10-04 17:47:32 -0700

    Define Object Properties and Methods

---
+++ app.js
@@ -0,0 +1,7 @@
+let playlist = new Playlist();
+
+let humoDeTabaco = new Song("Humo De Tabaco", "Alex Cuba", "3:28");
+let lovedLikeThis = new Song("Loved Like This", "Tatiana Manaois", "3:19");
+
+playlist.add(humoDeTabaco);
+playlist.add(lovedLikeThis);
